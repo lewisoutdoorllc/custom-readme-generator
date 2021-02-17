6cbdf539640dc6f7996959e28cad8065cd643f04
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-02-17 12:49:24 -0500

    added license

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+console.log('hello world');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
 
