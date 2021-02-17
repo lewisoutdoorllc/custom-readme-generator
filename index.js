91ad9b3187a63c11b0092d0bf54a3cdf30dcaac1
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-02-17 17:33:38 -0500

    changes to the template

--- index.js
+++ index.js
@@ -19,154 +19,181 @@ console.log(`
 //const questions = [];
 const promptUser = () => {
     return inquirer.prompt([
-      {
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+              if (descriptionInput) {
+                return true;
+              } else {
+                console.log('You need to enter a project description!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions for this project. (Required)',
+            validate: installationInput => {
+              if (installationInput) {
+                return true;
+              } else {
+                console.log('Please enter a name for your application!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'repoLink',
+            message: 'Please provide the GitHub link for this application. (Required)',
+            validate: repoLinkInput => {
+              if (repoLinkInput) {
+                return true;
+              } else {
+                console.log('Please enter a GitHub link for your application!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'gitHubUserName',
+            message: 'Please enter your GitHub username. (Required)',
+            validate: gitHubUserNameInput => {
+              if (gitHubUserNameInput) {
+                return true;
+              } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address. (Required)',
+            validate: emailInput => {
+              if (emailInput) {
+                return true;
+              } else {
+                console.log('Please enter your email address!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'credits',
+            message: 'Please enter any idividuals or sources that helped in the making of this application. (Required)',
+            validate: creditsInput => {
+              if (creditsInput) {
+                return true;
+              } else {
+                console.log('Please enter information to proceed!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What languages did you use to build this application? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+          },
+          {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide a description of how to use this application (Required)',
+            validate: usageInput => {
+              if (usageInput) {
+                return true;
+              } else {
+                console.log('Please enter a application description!');
+                return false;
+              }
+            }
+          },
+    {
         type: 'input',
-        name: 'name',
-        message: 'What is your name? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
+        name: 'screenshot',
+        message: 'Please provide a screenshot of the application. (Required)',
+        validate: screenshotInput => {
+          if (screenshotInput) {
             return true;
           } else {
-            console.log('Please enter your name!');
+            console.log('Please add a sceenshot of your application!');
             return false;
           }
         }
       },
       {
         type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username (Required)',
-        validate: githubInput => {
-          if (githubInput) {
+        name: 'test',
+        message: 'Please provide information on how we can test this application. (Required)',
+        validate: testInput => {
+          if (testInput) {
             return true;
           } else {
-            console.log('Please enter your GitHub username!');
+            console.log('Please provide information on how to test this application!');
             return false;
           }
         }
       },
       {
-        type: 'confirm',
-        name: 'confirmAbout',
-        message: 'Would you like to enter some information about yourself for an "About" section?',
-        default: true
-      },
-      {
-        type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => confirmAbout
-      }
-    ]);
-  };
-  
-  const promptProject = portfolioData => {
-    console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
-  
-    // If there's no 'projects' array property, create one
-    if (!portfolioData.projects) {
-      portfolioData.projects = [];
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license from the list below:',
+        choices: [
+            'None',
+            'Apache License 2.0',
+            'GNU General Public License v3.0',
+            'MIT License',
+            "BSD 2-Clause 'Simplified' License",
+            "BSD 3-Clause 'New' or 'Revised' License",
+            'Boost Software License 1.0',
+            'Creative Commons Zero v1.0 Universal',
+            'Eclipse Public License 2.0',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License v2.0',
+            'GNU Lesser General Public License v2.1',
+            'Mozilla Public License 2.0',
+            'The Unlicense',
+        ],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('You need to enter license option.');
+                return false;
+            }
+        }
     }
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'What is the name of your project? (Required)',
-          validate: nameInput => {
-            if (nameInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project name!');
-              return false;
-            }
-          }
-        },
-        {
-          type: 'input',
-          name: 'description',
-          message: 'Provide a description of the project (Required)',
-          validate: descriptionInput => {
-            if (descriptionInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project description!');
-              return false;
-            }
-          }
-        },
-        {
-          type: 'checkbox',
-          name: 'languages',
-          message: 'What did you this project with? (Check all that apply)',
-          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-        },
-        {
-          type: 'input',
-          name: 'link',
-          message: 'Enter the GitHub link to your project. (Required)',
-          validate: linkInput => {
-            if (linkInput) {
-              return true;
-            } else {
-              console.log('You need to enter a project GitHub link!');
-              return false;
-            }
-          }
-        },
-        {
-          type: 'confirm',
-          name: 'feature',
-          message: 'Would you like to feature this project?',
-          default: false
-        },
-        {
-          type: 'confirm',
-          name: 'confirmAddProject',
-          message: 'Would you like to enter another project?',
-          default: false
-        }
-      ])
-      
+    ])
+}; 
+
+function generateReadMe() {
+    try {
+        // Ask user questions and generate responses
+        const data = await promptUser();
+        const generateReadme = generateReadme(data);
+        // Write new README.md to dist directory
+        await writeReadMe('./dist/README.md', generateReadMe);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  generateReadMe();  
 
 
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-
-.then(projectData => {
-    portfolioData.projects.push(projectData);
-    if (projectData.confirmAddProject) {
-      return promptProject(portfolioData);
-    } else {
-      return portfolioData;
-    }
-  });
-};
-
-promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  return generateReadMe(portfolioData);
-})
-.then(pageHTML => {
-  return writeFile(pageHTML);
-})
-.then(writeFileResponse => {
-  console.log(writeFileResponse);
-  return copyFile();
-})
-.then(copyFileResponse => {
-  console.log(copyFileResponse);
-})
-.catch(err => {
-  console.log(err);
-});
 
 
 // Function call to initialize app
--- src/ReadMe-template.js
+++ src/ReadMe-template.js
@@ -1,86 +1,53 @@
 // TODO: Create a function to generate markdown for README
 function generateReadme(data) {
-    return ` 
+  return ` 
   # ${data.title} Lewis-Weather-Dashboard 
-  
-  ## ${data.tableOfContents}Table of Contents 
+  // ------ INSERT BADGE HERE FOR LICENSE  ------------------------------------ //
+  ## ${data.tableOfContents} Table of Contents 
   
   * [Description](#description)
   * [Installation](#installation)
-  * [Html](#html)
-  * [CSS](#css)
-  * [JavaScript](#javascript)
-  * [Link to site](#link-to-site)
+  * [Languages](#languages-used-for-application-development)
+  * [Usage](#usage)
   * [Screenshot](#screenshot-of-landing-page)
+  * [Test](#test)
+  * [Questions](#questions)
   * [Credits](#credits)
   * [License](#license)
   
   ## Description
-  This weather dashboard was made to allow the user to obtain the current weather for a city of there choice and provide a 5 day future forcast so they can plan there travel. It provides the user the ability to see current temperature, humidity, wind speed, and uv index; the uv index is also indicated by green, yellow, and red depending on severity. The future 5 day forcast provides the user with the dates, weather conditions, temperatures, and humidity. Finally the search history of the user is logged and displayed on the right side in a list that is populated based on the users searches. 
+  ${data.description}
   
   ## Installation
-  
-  Click the link to site in this README and it will take you to the landing page for the site. 
+  ${data.installation}
+  Link to GitHub Repository:  [${data.repoLink}](https://github.com/${data.gitHubUserName}/${data.repoLink})  
   
   ## Lauguages Used For Application Development
-  // Html//
+  ${data.languages}
   
-  Used bootstrap to lay out the rows and colums and some styles with the best of my ablity; any changes in the future will be updated here...
-  
-  ## CSS
-  /* CSS */ 
-  
-  Styles were inputted to make the user experience more enjoyable and to provide a clean apperance; any changes in the future will be updated here...
-  
-  ## JavaScript
-  
-  JavaScript along with implementing jquery is were we implemented all the working of the weather dashboard so the information could be inputted, saved, stored, and recalled when needed. 
-  
-  ## Link to site
-  
-  https://lewisoutdoorllc.github.io/Lewis-Weather-Dashboard/
-  
-  Link to GitHub Repo
-  https://github.com/lewisoutdoorllc/Lewis-Weather-Dashboard
-  
-  ## Screenshot of landing page
-  
-  ![alt text](./assets/images/screenShot1.png)
-  
+   ##Usage
+  ${data.usage}
+
+  ## Screenshot of Application
+  ![alt text](${data.screenShot})
+
+  ## Test
+  ${data.test}
+  ## Questions
+  //-------------------------- GitHub ProFile Link Here --------------------------------------------//
+  GitHub Link: ${data.gitHubUserName}(https://github.com/${data.gitHubUserName})
+
+  Contact via Email: ${data.email}
+
+  Contact me by Email
   ## Credits
-  
-  Thanks for all the help from Kip, Nick, Carlos, and Anthony on this little venture for making this happen.. Always fun!! Cheers..
-  
-  //  Links that helped get to the proper conclusion for providing the solutions to our users requirements..
-  
-  Kip, Nick, Carlos, and Anthony
-  
+  ${data.credits}
+
   ## License
-  
-  Copyright (c) 2012-2020 Scott Chacon and others
-  
-  Permission is hereby granted, free of charge, to any person obtaining
-  a copy of this software and associated documentation files (the
-  "Software"), to deal in the Software without restriction, including
-  without limitation the rights to use, copy, modify, merge, publish,
-  distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so, subject to
-  the following conditions:
-  
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
-  
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-  
+  ${data.license}
+  // ------ INSERT BADGE HERE FOR LICENSE  --------------------------------------- //
+  ${renderLicenseText(data)}
     `;
-  }
-  
-  
-  
-  module.exports = generateReadme;
+};
+
+module.exports = generateReadme;
